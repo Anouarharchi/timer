e5a12ae8c3b7dd0d2f66309c77731b204f2b0ee3
anouarharchi <anwarharchi003@gmail.com>
2024-11-14 18:12:57 +0100

    v1.1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Test from './component/test';
+import Test from './component/timer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
